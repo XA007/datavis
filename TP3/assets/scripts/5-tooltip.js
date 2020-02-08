2fa767ff30b73d6ffaee272d5846e4073cc98c08
Anwar <neoboujja@gmail.com>
2020-02-07 23:25:00 -0500

    added tooltip

--- TP3/assets/scripts/5-tooltip.js
+++ TP3/assets/scripts/5-tooltip.js
@@ -15,5 +15,12 @@
 function getToolTipText(d, formatNumber) {
   // TODO: Retourner le texte à afficher dans l'infobulle selon le format demandé.
   //       Assurez-vous d'utiliser la fonction "formatNumber" pour formater les nombres correctement.
-
+  const end = "<br/>"
+  const country_row = "Pays: " + d.name.bold() + end
+  const life_expectancy_row = "Espérance de vie: " + formatNumber(d.lifeExpectancy).bold() + " ans" + end
+  const income_row = "Revenu: " + formatNumber(d.income).bold() + " USD" + end
+  const population_row = "Population: " + formatNumber(d.population).bold() + " habitans" + end
+  const zone_row = "Zone du monde: " + d.zone.bold() + end
+  const hoverBox = country_row + life_expectancy_row + income_row + population_row + zone_row
+  return hoverBox
 }
