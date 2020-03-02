06332f654fda4856d2b69eae5102e1f205fe88b6
Youssef <youssef.ben-taleb@polymtl.ca>
2020-03-02 01:08:28 -0500

    initTileLayer function

--- TP5/assets/scripts/2-map.js
+++ TP5/assets/scripts/2-map.js
@@ -25,6 +25,16 @@ function initTileLayer(L, map) {
        - Niveau de zoom: 4.
    */
 
+  const coordinates = [57.3, -30.7]
+  const zoom = 4
+  map.setView(coordinates)
+  map.setZoom(zoom)
+
+  const urlTemplate = 'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'
+  const maxZoom = 10
+  const minZoom = 1
+  L.tileLayer(urlTemplate,{maxZoom,minZoom}).addTo(map)
+
 }
 
 /**
